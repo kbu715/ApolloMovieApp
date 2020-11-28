63e843742e52ff3adc798be24689b891e18ed7e2
kbu715 <kbu715@gmail.com>
2020-11-28 19:23:15 +0900

    Apollo Client GET MOVIES QUERY

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,35 @@
+import { useParams } from 'react-router-dom';
 import React from 'react';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
-const Detail = () => <h1>Detail</h1>
+//query 작성
+//query에 variable이 있을 때(id같은거) 그 query의 이름을 적어야한다. only for Apollo.
+const GET_MOVIE = gql` 
+    query getMovie($id: Int!){
+        movie(id: $id) { 
+            id
+            title
+            medium_cover_image
+            description_intro
+        }
+    }
+`;
+
+//query 안에 movie(id: $id) 이부분 부터 server을 위한 query.
+
+const Detail = () => {
+    const { id } = useParams();
+    const { loading, data } = useQuery(GET_MOVIE, {
+        variables: { id: parseInt(id) }
+    });
+
+    if(loading) {
+        return 'loading...';
+    } 
+    if(data && data.movie) {
+        return data.movie.title;
+    }
+}
 
 export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,71 @@
-import React from 'react';
+import React from "react";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import Movie from "../components/Movie";
+import styled from "styled-components";
 
-const Home = () => <h1>Home</h1>
+//query 작성
+const GET_MOVIES = gql`
+  {
+    movies {
+      id
+      medium_cover_image
+    }
+  }
+`;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd732a);
+  height: 50vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+    font-size: 35px;
+`;
+
+
+const Home = () => {
+  const { loading, data } = useQuery(GET_MOVIES);
+  console.log(data);
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2020</Title>
+        <Subtitle>I love Graphql</Subtitle>
+      </Header>
+
+      {loading && <Loading>Loading...</Loading>}
+      {!loading &&
+        data.movies &&
+        data.movies.map((movie) => <Movie key={movie.id} id={movie.id} />)}
+    </Container>
+  );
+};
 
 export default Home;
